0202fc642610f10e8ae6ebb1f69249bd40a818cb
DanielZT29 <80940197+DanielZT29@users.noreply.github.com>
2023-03-08 20:06:44 -0600

    Delete Funciones directory

--- Funciones/Conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli("localhost","root","","sicad","3308");
-	
-	if($conn->connect_errno)
-	{
-		echo "No hay conexión: (" . $conn->connect_errno . ") " . $conn->connect_error;
-	}
